22e75bc24e299516e280040beb2a2f630428a0b0
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-04-12 17:02:38 -0400

    ink.

---
+++ js/config.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2wxd204MXBnMDB4ODNwcXE2dGp3Y3U2bCJ9.HIKg5ahnyydbXMlrWH1f7Q";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/dark-v10",
+  center: [-96.052335, 39.159882],
+  zoom: 3.5
+});
