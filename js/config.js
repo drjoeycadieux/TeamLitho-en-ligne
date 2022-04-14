3a6eda0dddd2811414b6e53aca5c18cbe71ce1c8
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-04-14 16:13:28 -0400

    inm.

--- js/config.js
+++ js/config.js
@@ -2,7 +2,10 @@ mapboxgl.accessToken =
   "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2wxd204MXBnMDB4ODNwcXE2dGp3Y3U2bCJ9.HIKg5ahnyydbXMlrWH1f7Q";
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/dark-v10",
-  center: [-96.052335, 39.159882],
-  zoom: 3.5
+  style: "mapbox://styles/mapbox/navigation-day-v1",
+  center: [-73.567256, 45.5016889],
+  zoom: 9.0
 });
+
+// disable map zoom when using scroll
+map.scrollZoom.disable();
